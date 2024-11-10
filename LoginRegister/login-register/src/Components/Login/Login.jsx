f540474adaac904c366bb9a7719b8adc1a099e8e
daxpanic <daxpanic@yahoo.com>
2024-11-10 16:28:10 +0200

    changing login, register and forgot password view page logic

--- LoginRegister/login-register/src/Components/Login/Login.jsx
+++ LoginRegister/login-register/src/Components/Login/Login.jsx
@@ -4,106 +4,111 @@ import { FaUser, FaLock, FaEnvelope } from "react-icons/fa";
 
 const Login = () => {
 
-  const [action, setAction] = useState('activeLogin');
+  const [action, setAction] = useState('');
 
   const registerLink = () => {
-    setAction('activeRegister');
+    setAction('active');
   };
 
   const loginLink = () => {
-    setAction(`activeLogin`);
+    setAction(``);
   };
 
   const forgotPassword = () => {
-    setAction(`activeForgotPassword`);
+    setAction(``);
   }
 
   return (
-    <div className="forms-view-wrapper">
-      <div className="forms-wrapper">
-        {action === 'activeLogin' && (
-          <div className='form-box login'>
-            <form action="">
-              <h1>Login</h1>
-              <div className="input">
-                <input type="text" name="Username" placeholder='Username' required />
-                <FaUser className='icon' />
-              </div>
-
-              <div className="input">
-                <input type="password" name="Password" placeholder='Password' required />
-                <FaLock className='icon' />
-              </div>
-
-              <div className="remember-forgot">
-                <label><input type="checkbox" />Remember me</label>
-                <a href="#" onClick={forgotPassword}>Forgot password?</a>
-              </div>
-
-              <button type='submit'>Login</button>
-
-              <div className="register-link">
-                <p>Don't have an account? <a href="#" onClick={registerLink}>Register</a></p>
-              </div>
-            </form>
+    <div className={`wrapper ${action}`}>
+      <div className='form-box login'>
+        <form action="">
+          <h1>Login</h1>
+          <div className="input">
+            <input type="text" name="Username"
+              placeholder='Username' required />
+            <FaUser className='icon' />
           </div>
-        )}
 
-        {action === 'activeRegister' && (
-          <div className='form-box register'>
-            <form action="">
-              <h1>Registration</h1>
-              <div className="input">
-                <input type="text" name="Username" placeholder='Username' required />
-                <FaUser className='icon' />
-              </div>
-
-              <div className="input">
-                <input type="email" name="Email" placeholder='Email' required />
-                <FaEnvelope className='icon' />
-              </div>
-
-              <div className="input">
-                <input type="password" name="Password" placeholder='Password' required />
-                <FaLock className='icon' />
-              </div>
-
-              <div className="input">
-                <input type="password" name="Confirm password" placeholder='Confirm password' required />
-                <FaLock className='icon' />
-              </div>
-
-              <div className="remember-forgot">
-                <label><input type="checkbox" />I agree to Terms and Conditions</label>
-              </div>
-
-              <button type='submit'>Register</button>
-
-              <div className="register-link">
-                <p>Already have an account? <a href="#" onClick={loginLink}>Login</a></p>
-              </div>
-            </form>
+          <div className="input">
+            <input type="password" name="Password"
+              placeholder='Password' required />
+            <FaLock className='icon' />
           </div>
-        )}
 
-        {action === 'activeForgotPassword' && (
-          <div className='form-box forgotPassword'>
-            <form action="">
-              <h1>Recovery</h1>
-              <div className="input">
-                <input type="text" name="Email" placeholder='Email' required />
-                <FaEnvelope className='icon' />
-              </div>
-              <button type='submit'>Submit</button>
-              <div className="login-link">
-                <p>Remembered your password? <a href="#" onClick={loginLink}>Login</a></p>
-              </div>
-            </form>
+          <div className="remember-forgot">
+            <label><input type="checkbox" />Remember me</label>
+            <a href="#" onClick={forgotPassword}>Forgot password?</a>
           </div>
-        )}
+
+          <button type='submit'>Login</button>
+
+          <div className="register-link">
+            <p>Don't have and account? <a href="#" onClick=
+              {registerLink}>Register</a></p>
+          </div>
+
+        </form>
+      </div>
+
+      <div className='form-box register'>
+        <form action="">
+          <h1>Registration</h1>
+          <div className="input">
+            <input type="text" name="Username"
+              placeholder='Username' required />
+            <FaUser className='icon' />
+          </div>
+
+          <div className="input">
+            <input type="email" name="Email"
+              placeholder='Email' required />
+            <FaEnvelope className='icon' />
+          </div>
+
+          <div className="input">
+            <input type="password" name="Password"
+              placeholder='Password' required />
+            <FaLock className='icon' />
+          </div>
+
+          <div className="input">
+            <input type="password" name="Confirm password"
+              placeholder='Confirm password' required />
+            <FaLock className='icon' />
+          </div>
+
+          <div className="remember-forgot">
+            <label><input type="checkbox" />I agree to Terms and Conditions</label>
+          </div>
+
+          <button type='submit'>Register</button>
+
+          <div className="register-link">
+            <p>Already have an account? <a href="#" onClick=
+              {loginLink}>Login</a></p>
+          </div>
+
+        </form>
+      </div>
+
+      <div className='form-box forgotPassword'>
+        <form action="">
+          <h1>Recovery</h1>
+          <div className="input">
+            <input type="text" name="Email"
+              placeholder='Email' required />
+            <FaEnvelope className='icon' />
+          </div>
+          <button type='submit'>Submit</button>
+          <div className="login-link">
+            <p>Remembered your password? <a href="#" onClick=
+              {loginLink}>Login</a></p>
+          </div>
+        </form>
       </div>
     </div>
-  );
-};
+
+  )
+}
 
 export default Login
